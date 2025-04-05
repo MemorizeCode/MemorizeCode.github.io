fedd8c42da0f87544da12009e69a7c72724dd9da
MemorizeCode <ilya.bezrukov2005@mail.ru>
2025-04-05 18:11:31 +0400

    fix pag

--- dev/src/shared/Pagination/ui/AppPagination.vue
+++ dev/src/shared/Pagination/ui/AppPagination.vue
@@ -23,19 +23,38 @@ const rightArrow = computed(() => (color_theme.value === 'dark' ? right : right_
 
 const updatePages = () => {
   const { page, maxPage } = props;
+  console.log(page, maxPage);
   pages.value = [];
 
-  if (maxPage <= 5) {
-    for (let i = 1; i <= maxPage; i++) {
+  if (page < 4) {
+    for (let i = 1; i <= 4; i++) {
       pages.value.push(i);
     }
-  } else {
-    if (page <= 3) {
-      pages.value.push(1, 2, 3, '...', maxPage);
-    } else if (page >= maxPage - 2) {
-      pages.value.push(1, '...', maxPage - 2, maxPage - 1, maxPage);
-    } else {
-      pages.value.push(1, '...', page - 1, page, page + 1, '...', maxPage);
+    pages.value.push('...');
+    pages.value.push(maxPage);
+  }
+  else {
+    if (page >= maxPage || page >= maxPage - 1) {
+      pages.value.push(1);
+      pages.value.push('...');
+      pages.value.push(maxPage - 1);
+      pages.value.push(maxPage);
+    }
+    else if (page >= maxPage - 1) {
+      pages.value.push(1);
+      pages.value.push('...');
+      pages.value.push(maxPage - 2);
+      pages.value.push(maxPage - 1);
+      pages.value.push(maxPage);
+    } 
+    else {
+      pages.value.push(1);
+      pages.value.push('...');
+      pages.value.push(page - 1);
+      pages.value.push(page);
+      pages.value.push(page + 1);
+      pages.value.push('...');
+      pages.value.push(maxPage);
     }
   }
 };
@@ -53,25 +72,23 @@ function incPage() {
     props.setPage(props.page + 1);
   }
 }
+
+
+
 </script>
 
 <template>
   <div class="solo_pagination">
     <!-- Используем динамические иконки для стрелок -->
     <img :src="leftArrow" :style="{ marginRight: '10px' }" @click="decPage" />
-    
-    <button
-      v-for="(page, index) in pages"
-      :key="index"
-      :class="{ active__page: props.page === page }"
-      @click="typeof page === 'number' && props.setPage(page)"
-      :disabled="page === '...'"
-    >
+
+    <button v-for="(page, index) in pages" :key="index" :class="{ active__page: props.page === page }"
+      @click="typeof page === 'number' && props.setPage(page)" :disabled="page === '...'">
       <span :class="{ active__page__text: props.page === page }">
         {{ page }}
       </span>
     </button>
-    
+
     <img :src="rightArrow" :style="{ marginLeft: '10px' }" @click="incPage" />
   </div>
 </template>
